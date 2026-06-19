fc08f5de5058c1886a7d27b6989cd30dbb5b89b8
Geniolle <consultorsapclaytonlopes@gmail.com>
2026-06-19 15:25:41 +0100

    fix: clean member user one-to-one branch

--- backups/fix_entity_default_label_estado_20260608_095351/entity_default_rule.js
+++
@@ -1,5 +0,0 @@
-﻿// Diagnóstico temporário
-// A regra Estado + Nº Entidade foi desativada temporariamente para testar travamento do browser.
-(function () {
-  console.log('APPVERBO_DIAGNOSTICO: entity_default_rule.js carregado em modo seguro/no-op.');
-})();
--- backups/migrar_sessoes_admin_subprocess_v2_20260505_154302/appverbo/admin_subprocesses/repositories/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .base import BaseAdminSubprocessRepository
-
-__all__ = ["BaseAdminSubprocessRepository"]
