e6af84efc92521c5666fa4c6eaf17c9523e63d12
Geniolle <consultorsapclaytonlopes@gmail.com>
2026-07-04 08:26:39 +0100

    Split i18n login runtime changes

--- appverbo/services/__init__.py
+++ appverbo/services/__init__.py
@@ -7,4 +7,5 @@ from appverbo.services.page import *  # noqa: F403,F401
 from appverbo.services.permissions import *  # noqa: F403,F401
 from appverbo.services.profile import *  # noqa: F403,F401
 from appverbo.services.session import *  # noqa: F403,F401
+from appverbo.services.i18n import *  # noqa: F403,F401
 from appverbo.services.whatsapp import *  # noqa: F403,F401
